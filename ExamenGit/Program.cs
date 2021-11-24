55cc483e1d55e641c4afcd8afcbadfb89b645854
MrDimaBahmetev <bahmetev168@gmail.com>
2021-11-24 16:34:48 +0600

    New method ShowRand

--- ExamenGit/Program.cs
+++ ExamenGit/Program.cs
@@ -7,6 +7,13 @@ namespace ExamenGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            ShowRand();
+        }
+
+        static void ShowRand()
+        {
+            Random rand = new Random();
+            Console.WriteLine(rand.Next(0,100));
         }
     }
 }
